9fea5f3ac5e3bd0a69224330c8147d089a864824
hunter <hunterjsb@gmail.com>
2025-04-27 22:33:23 -0400

    register migrate command in main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,32 +1,45 @@
 package main
 
 import (
-   "log"
+	"log"
+	"os"
+	"strings"
 
-   "github.com/pocketbase/pocketbase"
-   "github.com/pocketbase/pocketbase/core"
-   "github.com/pocketbase/pocketbase/tools/cron"
-   "github.com/we-be/vibecheckr/internal/rank"
+	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/core"
+	"github.com/pocketbase/pocketbase/plugins/migratecmd"
+	"github.com/pocketbase/pocketbase/tools/cron"
+
+	"github.com/we-be/vibecheckr/internal/rank"
 )
 
 func main() {
-   app := pocketbase.New()
+	app := pocketbase.New()
 
-   app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
-       scheduler := cron.New()
+	// loosely check if it was executed using "go run"
+	isGoRun := strings.HasPrefix(os.Args[0], os.TempDir())
 
-       // Calculate rank every 10 minutes
-       scheduler.MustAdd("calculate-rank", "*/10 * * * *", func() {
-           if err := rank.CalculateRank(app); err != nil {
-               log.Printf("Error calculating rank: %v", err)
-           }
-       })
+	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
+		scheduler := cron.New()
 
-       scheduler.Start()
-       return nil
-   })
+		migratecmd.MustRegister(app, app.RootCmd, migratecmd.Config{
+			// enable auto creation of migration files when making collection changes in the Dashboard
+			// (the isGoRun check is to enable it only during development)
+			Automigrate: isGoRun,
+		})
 
-   if err := app.Start(); err != nil {
-       log.Fatal(err)
-   }
+		// Calculate rank every 10 minutes
+		scheduler.MustAdd("calculate-rank", "*/10 * * * *", func() {
+			if err := rank.CalculateRank(app); err != nil {
+				log.Printf("Error calculating rank: %v", err)
+			}
+		})
+
+		scheduler.Start()
+		return nil
+	})
+
+	if err := app.Start(); err != nil {
+		log.Fatal(err)
+	}
 }
